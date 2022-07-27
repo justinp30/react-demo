89b7df9721afc9af455aa4e7a01418daa20624c2
Violet Cutler <violetcutler@Violets-MacBook-Air.local>
2022-07-27 13:06:37 -0400

    React Template Created

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(
+<Router>
+ <App/>
+</Router>
+
+);
